f4fad8f76324ed1a019107a1b2a9d20fca0b1a23
AbdulSalam AbdulRahman <abdulrahmanabdulsalam93@gmail.com>
2025-11-26 22:13:28 +0100

    fix: add validation rules to CreateUserDto

--- src/app/users/dto/create-user.dto.ts
+++ src/app/users/dto/create-user.dto.ts
@@ -1,1 +1,37 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Max,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName!: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  department!: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(6)
+  yearOfStudy!: number;
+}
